12041d602eb1ad1008bacc669bdff3ff4582f5be
sagar-sarvam <sagar@sarvam.ai>
2025-06-10 22:25:25 +0530

    upddate 2

--- start.py
+++ start.py
@@ -1,3 +1,16 @@
-from utils.general import greet
-
-greet()
+def add(n,m):
+    return m+n
+def factorial(n):
+    r=1
+    for i in range(n):
+        r*=i+1   
+    return r
+def mix(n):
+    l=add(n,2*n)
+    p=factorial(l)
+    j=p*10
+    import pdb;pdb.set_trace() 
+    return j*p/l
+        
+h=mix(2)        
+print(h)
--- utils/general.py
+++ utils/general.py
@@ -1,2 +1,4 @@
-def greet():
-    print("hello world")
+from .c_utils.replicate import replicate
+def greet(n:int):
+    k=replicate("hello world",5)
+    print(k)
